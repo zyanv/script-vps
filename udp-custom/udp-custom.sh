04ad102cd7f2c7afb44aedc0c094d53b4985a177
zyanv <azammuddinazrul18@gmail.com>
2025-06-13 22:19:19 +0700

    Update udp-custom.sh

--- udp-custom/udp-custom.sh
+++ udp-custom/udp-custom.sh
@@ -6,7 +6,7 @@ mkdir -p /etc/udp
 echo "change to time GMT+7"
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 cd /etc/udp
-wget -q -O udp-custom "https://github.com/hokagelegend9999/alpha.v2/raw/refs/heads/main/udp-custom/udp-custom-linux-amd64"
+wget -q -O udp-custom "https://raw.githubusercontent.com/zyanv/script-vps/refs/heads/main/udp-custom/udp-custom-linux-amd64"
 chmod +x udp-custom
 cd
 
