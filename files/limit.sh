23133492f9a9d21ff6aa0bf0167945b3a1b7262d
zyanv <azammuddinazrul18@gmail.com>
2025-06-13 21:48:27 +0700

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/hokagelegend9999/alpha.v2/refs/heads/main/"
+REPO="https://raw.githubusercontent.com/zyanv/script-vps/refs/heads/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
